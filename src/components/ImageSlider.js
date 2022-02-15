17c6b587673bb237d11d721ebefda4862f8abe05
Suresh Lakshete <Suresh.Lakshete@navigateconsulting.in>
2022-02-15 18:02:16 +0530

    Add viewer to the app.

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -43,13 +43,23 @@ const Carousel = styled(Slider)`
     }
 
     .slick-list {
-        overflow: visible;
-    }
+        overflow: initial;
+      }
 
     button {
         z-index: 1;
     }
 
+    & > button {
+        opacity: 0;
+        height: 100%;
+        width: 5vw;
+        z-index: 1;
+        &:hover {
+          opacity: 1;
+          transition: opacity 0.2s ease 0s;
+        }
+      }
 
 `
 
