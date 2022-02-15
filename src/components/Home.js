d610c4c2a50d95345bb74a513fb559bc65ce8bd1
Suresh Lakshete <Suresh.Lakshete@navigateconsulting.in>
2022-02-15 10:54:44 +0530

    Add image slider to the app.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+import ImageSlider from './ImageSlider';
 
 function Home() {
   return (
-    <div>
-        Home
-    </div>
+    <Container>
+        <ImageSlider />
+    </Container>
     )
 }
 
 export default Home;
+
+const Container = styled.main`
+    min-height: calc(100vh - 70px);
+    padding: 0 calc(3.5vw + px);
+    position: relative;
+    overflow: hidden;
+    &:before {
+        background: url("/images/home-background.png") center center / cover no-repeat fixed;
+        content: "";
+        position: absolute;
+        top: 0;
+        right: 0;
+        left: 0;
+        bottom: 0;
+        z-index: -1;
+    }
+`
